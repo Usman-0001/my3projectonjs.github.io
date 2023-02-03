3cfab9bbd861fe5d012229aee1f2370912acac91
Usman-0001 <106924264+Usman-0001@users.noreply.github.com>
2023-02-04 01:17:54 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const upBtn = document.querySelector('.up-button')
+
+const downBtn = document.querySelector('.down-button')
+
+const sidebar = document.querySelector('.sidebar')
+
+const mainSlide = document.querySelector('.main-slide')
+
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+const container = document.querySelector('.container')
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+
+// Улучшаем размер фото
+const width = container.clientWidth
+mainSlide.style.width = `${width - 450}px`
+// Улучшаем размер фото
+
+
+
+upBtn.addEventListener('click', () => {
+    changeSlide("up")
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide("down")
+})
+
+function changeSlide(direct) {
+    if (direct === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    } else if (direct === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+
+
+    const height = container.clientHeight
+
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
