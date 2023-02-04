7f9e076d6c485ee0752bceea62795a6048d890be
Usman-0001 <106924264+Usman-0001@users.noreply.github.com>
2023-02-04 21:07:51 +0600

    app.js

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ downBtn.addEventListener('click', () => {
     changeSlide("down")
 })
 
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowUp") {
+        changeSlide("up")
+    } else if (event.key === "ArrowDown") {
+        changeSlide("down")
+    }
+})
+
 function changeSlide(direct) {
     if (direct === 'up') {
         activeSlideIndex++
